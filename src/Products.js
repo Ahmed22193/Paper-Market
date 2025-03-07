48a73925627b1eefe6842ad43606c7ab353ffcba
Ahmed22193 <Ahmed221050193@gmail.com>
2025-03-08 00:23:07 +0200

    fix image path

--- src/Products.js
+++ src/Products.js
@@ -2,15 +2,16 @@ import { useState } from 'react';
 import './PRODUCT.css';
 
 export default function Products() {
+    // eslint-disable-next-line no-unused-vars
     const [cart, setcart] = useState([]);
     const products = [
-        { id: 1, name: 'اكواب ورق 12 اونز', price: '125 LE', image: './imgs/cups.jpg' },
-        { id: 2, name: 'رول ورق زبدة', price: '199 LE', image: './imgs/foilepaper.jpg' },
-        { id: 3, name: 'اكياس يد للتسوق', price: '99 LE', image: './imgs/marketpaper.jpg' },
-        { id: 3, name: 'ورق متعدد الاستخدامات', price: '65 LE', image: './imgs/porgerpaper.jpg' },
-        { id: 3, name: 'ورق تغليف المنتجات', price: '99 LE', image: './imgs/Vegitaplepaper.jpg' },
-        { id: 3, name: 'اكواب رمضانية حجم 6.5', price: '100 LE', image: './imgs/whiteCups.jpg' }
-    ];
+        { id: 1, name: 'اكواب ورق 12 اونز', price: '125 LE', image: `${process.env.PUBLIC_URL}/imgs/cups.jpg` },
+        { id: 2, name: 'رول ورق زبدة', price: '199 LE', image: `${process.env.PUBLIC_URL}/imgs/foilepaper.jpg` },
+        { id: 3, name: 'اكياس يد للتسوق', price: '99 LE', image: `${process.env.PUBLIC_URL}/imgs/marketpaper.jpg` },
+        { id: 4, name: 'ورق متعدد الاستخدامات', price: '65 LE', image: `${process.env.PUBLIC_URL}/imgs/porgerpaper.jpg` },
+        { id: 5, name: 'ورق تغليف المنتجات', price: '99 LE', image: `${process.env.PUBLIC_URL}/imgs/Vegitaplepaper.jpg` },
+        { id: 6, name: 'اكواب رمضانية حجم 6.5', price: '100 LE', image: `${process.env.PUBLIC_URL}/imgs/whiteCups.jpg` }
+    ];    
 
     const addToCart = (product) => {
         let cart = localStorage.getItem('myCart')
